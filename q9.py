e06c6d55a333f6fd1565c7d8e7f87313e3805014
kt54 <kt2693@columbia.edu>
2018-04-24 22:41:02 +0000

    Add a new file

---
+++ q9.py
@@ -0,0 +1,18 @@
+
+m flask import Flask
+
+from flask import render_template
+
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def quotes():
+
+        with open("inspiration.txt",encoding="utf8") as fp:
+            a=fp.readlines()
+        ran=random.randint(0,len(a)-1)
+        quote=a[ran].replace('\n','')
+        return render_template('quotes.html', quote=quote)
