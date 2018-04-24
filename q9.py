289753addaab6655dede30d9f521b8102da4f1e0
kt54 <kt2693@columbia.edu>
2018-04-24 22:47:50 +0000

    Modified the file

--- q9.py
+++ q9.py
@@ -1,5 +1,5 @@
 
-m flask import Flask
+from flask import Flask
 
 from flask import render_template
 
@@ -10,9 +10,8 @@ app = Flask(__name__)
 @app.route("/")
 
 def quotes():
-
-        with open("inspiration.txt",encoding="utf8") as fp:
-            a=fp.readlines()
-        ran=random.randint(0,len(a)-1)
-        quote=a[ran].replace('\n','')
-        return render_template('quotes.html', quote=quote)
+    with open("inspiration.txt",encoding="utf8") as fp:
+        a=fp.readlines()
+    ran=random.randint(0,len(a)-1)
+    quote=a[ran].replace('\n','')
+    return render_template('quotes.html', quote=quote)
